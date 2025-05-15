688bc9d87a92cb8fcbf9b95ff9b8b4c6494dd8b0
MattPChoy <matthewpchoy@gmail.com>
2025-05-15 20:46:37 +1000

    Feat: Posts a cute Gif when the bot wakes up!

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+@bot.event
+async def on_ready():
+    channel_id = 1167760258215714817  # Replace with your channel ID
+    channel = bot.get_channel(channel_id)
+    if channel:
+        with open('./assets/anya.gif', 'rb') as f:
+            await channel.send(file=discord.File(f, 'anya.gif'))
+
 async def setup_bot():
 	await bot.add_cog(Uptime())
 
