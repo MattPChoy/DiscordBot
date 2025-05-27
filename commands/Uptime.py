21d3133749f274ec79b9ca28447b1e9e6287b36e
MattPChoy <matthewpchoy@gmail.com>
2025-05-27 22:25:19 +1000

    Fix: Cleanup and refactor the uptime command.

--- commands/Uptime.py
+++ commands/Uptime.py
@@ -3,12 +3,11 @@ import datetime
 
 class Uptime(commands.Cog):
     def __init__(self):
-        self.start_time = datetime.datetime.now(datetime.timezone.utc)
+        self.start_time = datetime.datetime.now()
 
     @commands.command()
     async def uptime(self, ctx):
-        now = datetime.datetime.now(datetime.timezone.utc)
-        delta = now - self.start_time
+        delta = datetime.datetime.now() - self.start_time
         hours, remainder = divmod(int(delta.total_seconds()), 3600)
         minutes, seconds = divmod(remainder, 60)
         await ctx.send(f"Uptime: {hours}h {minutes}m {seconds}s")
