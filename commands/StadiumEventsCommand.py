374baaefb0219f55017d838f2bb98f7e1f839a55
MattPChoy <matthewpchoy@gmail.com>
2025-06-07 20:03:19 +1000

    fix: validation on number of days, and add filtering by venue type

--- commands/StadiumEventsCommand.py
+++ commands/StadiumEventsCommand.py
@@ -1,23 +1,42 @@
 from discord.ext import commands
 from scrapers.StadiumEventSchedule import get_events
-
+import datetime
 
 class SuncorpEventsCommand(commands.Cog):
     @commands.hybrid_command(name="events")
-    async def __stadium_events__(self, ctx, n_days=3):
-        events=get_events(n_days)
-        await self.send_stadium_events(ctx, events, n_days)
+    async def __stadium_events__(self, ctx, n_days:str="3", venue=""):
 
+        try :
+            days = int(n_days)
+        except ValueError:
+            await ctx.reply(f"\"{n_days}\" is not a valid number of days.")
+            return
+        
+        MAX_DAYS = (datetime.date.max - datetime.date.today()).days
+
+        if days > MAX_DAYS:
+            await ctx.reply(f"number of days must be less than {MAX_DAYS}")
+            return
+
+        if days < 1:
+            await ctx.reply("Are you a time traveller?")
+            return
+
+        events=get_events(days)
+        await self.send_stadium_events(ctx, events, days, venue)
 
     @staticmethod
-    async def send_stadium_events(ctx, events, n_days=3):
+    async def send_stadium_events(ctx, events, n_days, venue=""):
         suncorp_icon = ":sun_with_face:"
-        gabba_icon = "🐑"
+        gabba_icon = "🏟"
 
         message = f"# Stadium events in the next {n_days} days:\n" if n_days != 1 else "# Stadiums events today:\n"
         message += f"{suncorp_icon}: Suncorp, {gabba_icon}: Gabba\n\n"
 
         for event in events:
+            if venue.lower() in ["suncorp", "gabba"]:
+                if event.location.lower() != venue.lower():
+                    continue
             event_symbol = suncorp_icon if event.location == 'Suncorp' else gabba_icon
             event_date = f"📅 {event.date.strftime('%a %d %b')}"
             event_time = ""
@@ -28,11 +47,10 @@ class SuncorpEventsCommand(commands.Cog):
 
         await ctx.send(message)
 
-    @staticmethod
-    async def daily_message(channel):
+    async def daily_message(self, channel):
         events = get_events(1)
 
         if len(events) == 0:
             return
 
-        await SuncorpEventsCommand.send_stadium_events(channel, events, 1)
+        await SuncorpEventsCommand.send_stadium_events(channel, events, "1")
